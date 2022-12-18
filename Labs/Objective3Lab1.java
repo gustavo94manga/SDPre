695e5916880854c9439abd9f8f20d83adce15963
Angel Garza <gustavo94manga@gmail.com>
2022-12-17 19:23:24 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,6 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1 = 15, num2 = 8, num3 = 4, average = num1 + num2 + num3 / 3;
+		System.out.println(average);
+	}
+}
