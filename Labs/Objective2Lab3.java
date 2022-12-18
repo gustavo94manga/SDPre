60d8af8458e26eb32f32e56712e3a16c977ba3fb
Angel Garza <gustavo94manga@gmail.com>
2022-12-17 19:22:06 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1998;
+		String month = "January";
+		boolean weekend = true;
+		boolean weekday = false;
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(weekend);
+	}
+}
