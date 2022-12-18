d696523709e4533dca3488d11f94c1e7e7a98d3d
Angel Garza <gustavo94manga@gmail.com>
2022-12-17 19:20:51 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Angel Garza";
+		int age = 24;
+		String country = "Mexico";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(country);
+
+	}
+}
