7ebd53ef47941bbcbe92a2dc154efcaa68d9f95a
Angel Garza <gustavo94manga@gmail.com>
2022-12-20 09:31:30 -0600

    complete Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,15 @@
+public class Objective5Lab1 {
+	public static void main(String[] args) {
+
+		int num1 = 5;
+		int num2 = 20;
+
+
+		if(num1 > num2) {
+			System.out.println("5 is greater than 20.");
+		}
+		else if(num1 < num2) {
+			System.out.println("5 is less than 20, or 20 is greater than 5.");
+		}
+	}
+}
