f8da1e7db104f6f53f69490ada2672c1f160698a
Angel Garza <gustavo94manga@gmail.com>
2022-12-21 11:31:56 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int total = 210, sum = 0;
+
+		for (int i = 1; i <= 20; i++) {
+			sum += i;
+		}
+		System.out.println(total);
+	}
+}
