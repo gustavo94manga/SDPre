f229e92571062bfd7a723d531dfd263e48864e0f
Angel Garza <gustavo94manga@gmail.com>
2022-12-21 08:21:18 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int counter = 1;
+		int product = 1;
+
+		while(counter <= 20) {
+			System.out.println(counter * 10);
+			counter = counter + 1;
+		}
+	}
+}
