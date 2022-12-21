268345924d6cf95be4a69a5717981e8fb0c7a0b3
Angel Garza <gustavo94manga@gmail.com>
2022-12-21 11:07:02 -0600

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		for (int a = 1; a <= 10; a++) {
+			System.out.println(a);
+		}
+		for (int a = 10; a >= 1; a--) {
+			System.out.println(a);
+		}
+	}
+}
